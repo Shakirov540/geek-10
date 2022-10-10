3150c3569904b6cd0a428a93fad7edafcc22aaec
Aibek <ashakirov981@gmail.com>
2022-10-10 16:17:59 +0600

    test

--- geek 10/main.swift
+++ geek 10/main.swift
@@ -15,36 +15,10 @@ import Foundation
 //
 //Создать программу, которая сгруппирует слова, введенные в readLine по первой букве в отдельные массивы внутри одного массива (15 баллов)
 
-
-
-//Nomer
-//func uslovie(n1: Int, n2: Int, n3: Int, n4: Int, n5: Int, n6: Int) {
-//    if n1 > n2 && n1 > n3 && n1 > n4 && n1 > n5 && n1 > n6{
-//        print("Chislo ot malenkogo do bolshogo \(n1)")
-//
-//    }else  if n2 > n1 && n2 > n3 && n2 > n4 && n2 > n5 && n2 > n6{
-//        print("eto chislo bolshe  \(n2)")
-//}else  if n3 > n2 && n3 > n1 && n3 > n4 && n3 > n5 && n3 > n6{
-//    print("eto chislo bolshe \(n3)")
-//}else  if n4 > n2 && n4 > n3 && n4 > n1 && n4 > n5 && n4 > n6{
-//    print("eto chislo bolshe  \(n4)")
-//}else  if n5 > n2 && n5 > n3 && n5 > n4 && n5 > n1 && n5 > n6{
-//    print("eto chislo bolshe  \(n5)")
-//}else  if n6 > n2 && n6 > n3 && n6 > n4 && n6 > n5 && n6 > n1{
-//    print("eto chislo bolshe  \(n6)")
-//}
-//}
-//var n1 = readLine()!
-//var n2 = readLine()!
-//var n3 = readLine()!
-//var n4 = readLine()!
-//var n5 = readLine()!
-//var n6 = readLine()!
-//uslovie(n1: Int(n1) ?? 1 , n2: Int(n2) ?? 55, n3:  Int(n3) ?? 33, n4:  Int(n4) ?? 44, n5:  Int(n5) ?? 63, n6:  Int(n6) ?? 99)
 //nomer 2
 var symbols = 0
 var probely = 0
-func bhbyb(a:String){
+func bvb(a:String){
     for i in a{
         if i != " "{
             symbols+=1
@@ -56,23 +30,23 @@ func bhbyb(a:String){
     print(probely)
 }
 var aa = readLine()!
-bhbyb(a: aa)
+bvb(a: aa)
 //Nomer 3
 var arrayarray : [[String]] = [[String]]()
 func gruppslova(slovo:String){
     let first = slovo.first!
-    var fghjk = false
+    var bmw = false
     for (index,item) in arrayarray.enumerated(){
         for (index2,item2) in item.enumerated(){
-            if index2 == 0 && item2 == String(first) && fghjk == false{
+            if index2 == 0 && item2 == String(first) && bmw == false{
                 arrayarray[index].append(slovo)
-                fghjk = true
+                bmw = true
             }else{
                 continue
             }
         }
     }
-    if fghjk == false{
+    if bmw == false{
         var arrayy: [String] = []
         arrayy.append(String(first))
         arrayy.append(slovo)
@@ -81,8 +55,6 @@ func gruppslova(slovo:String){
 
     
     print(arrayarray)
-    
-    
     
 }
 gruppslova(slovo:readLine()!)
